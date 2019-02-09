5b6490e3b30a264096cb44e81508fa6764cb8725
Andrew McLaren <apm59@pitt.edu>
2019-02-09 17:06:44 -0500

    Init

---
+++ QueueUp/MainViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QueueUp
+//
+//  Created by Andrew McLaren on 2/9/19.
+//  Copyright © 2019 AMcLaren. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
+
